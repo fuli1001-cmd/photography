a434bb697b57fed9b025e6aa16fc782ec10a5567
david <david.fu@siloon.com>
2020-05-08 23:18:15 +0800

    add PostController

---
+++ Src/Services/Post/Post.API/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Photography.Services.Post.API.Application.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Photography.Services.Post.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    public class PostController : ControllerBase
+    {
+        private readonly ILogger<PostController> _logger;
+        private readonly IPostQueries _postQueries;
+        private readonly IMediator _mediator;
+
+        public PostController(IMediator mediator, IPostQueries postQueries, ILogger<PostController> logger)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _postQueries = postQueries ?? throw new ArgumentNullException(nameof(postQueries));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
