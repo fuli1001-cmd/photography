003a17e0aa36704838e3b1cfa4a5ae4036d6d24a
david <david.fu@siloon.com>
2020-05-13 22:12:27 +0800

    GetFollowedPosts

--- Src/Services/Post/Post.API.Query.EF/PostQueries.cs
+++ Src/Services/Post/Post.API.Query.EF/PostQueries.cs
@@ -44,70 +44,38 @@ namespace Photography.Services.Post.API.Query.EF
 
         private async Task<List<PostViewModel>> GetHotPostsAsync()
         {
-            //_postContext.Posts.Select(p =>
-            //{
-            //    new PostViewModel
-            //    {
-            //        Id = p.Id,
-            //        Text = p.Text,
-            //        LikeCount = p.LikeCount,
-            //        ShareCount = p.ShareCount,
-            //        CommentCount = p.CommentCount,
-            //        Timestamp = p.Timestamp,
-            //        Commentable = p.Commentable ?? false,
-            //        ForwardType = p.ForwardType,
-            //        ShareType = p.ShareType,
-            //        ViewPassword = p.ViewPassword,
-            //        Location = p.Location,
-            //        PostAttachments
-            //    }
-            //})
-            var posts = await _postContext.Posts
-                .Include(p => p.User)
-                .Include(p => p.PostAttachments)
-                .Include(p => p.ForwardedPost)
-                .OrderByDescending(p => p.Points)
-                .ToListAsync();
-            return _mapper.Map<List<PostViewModel>>(posts);
+            return await GetPostViewModelsAsync(_postContext.Posts);
         }
 
         private async Task<List<PostViewModel>> GetFollowedPostsAsync()
         {
-            //var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-            //var posts = _postContext.Posts
-            //    .Join(_postContext.Users, p => p.UserId, u => u.Id, (p, u) => new { p, u })
-            //    .Join(_postContext.UserRelations, pu => pu.p.UserId, ur => ur.FollowedUserId, (pu, ur) => new { pu, ur })
-            //    .Where(pur => pur.ur.FollowerId.ToString() == userId)
-            //    .GroupJoin(_postContext.Posts, pur => pur.pu.p.ForwardedPostId, fp => fp.Id, (pur, fp) => new { pur, fp })
-            //    .GroupJoin(_postContext.PostAttachments, pur_fp => pur_fp.pur.pu.p.Id, pa => pa.PostId, (purfp, pa) => new { purfp, pa })
-            //    .SelectMany(pur_fp_pa =>
-            //    {
-            //        pur_fp_pa.pa.DefaultIfEmpty();
-            //        pur_fp_pa.purfp.fp.DefaultIfEmpty();
-            //    },
-            //    (pur_fp_pa, pa) => new PostViewModel
-            //    {
-            //        Id = pur_fp_pa.purfp.pur.pu.p.Id,
-            //        Text = pur_fp_pa.purfp.pur.pu.p.Text,
-            //        LikeCount = pur_fp_pa.purfp.pur.pu.p.LikeCount
-            //    });
+            var followedUserIds = _postContext.UserRelations
+                .Where(ur => ur.FollowerId.ToString() == userId)
+                .Select(ur => ur.FollowedUserId);
 
-            ////var posts = await (from p1 in _postContext.Posts
-            ////                   join u in _postContext.Users on p1.UserId equals u.Id
-            ////                   join ur in _postContext.UserRelations on p1.UserId equals ur.FollowedUserId
-            ////                   where ur.FollowerId.ToString() == userId
-            ////                   join p2 in _postContext.Posts
-            ////                   on p1.ForwardedPostId equals p2.Id
-            ////                   into pt
-            ////                   from p in pt.DefaultIfEmpty()
-            ////                   join a in _postContext.PostAttachments
-            ////                   on 
-            ////             orderby p1.Points
-            ////                   select p1).ToListAsync();
+            var queryablePosts = _postContext.Posts.Where(p => followedUserIds.Contains(p.UserId));
 
-            //return _mapper.Map<List<PostViewModel>>(posts);
-            return null;
+            return await GetPostViewModelsAsync(queryablePosts);
+        }
+
+        //private IQueryable<Domain.AggregatesModel.PostAggregate.Post> FilterByVisibility(IQueryable<Domain.AggregatesModel.PostAggregate.Post> query)
+        //{
+            
+        //}
+
+        private async Task<List<PostViewModel>> GetPostViewModelsAsync(IQueryable<Domain.AggregatesModel.PostAggregate.Post> query)
+        {
+            var postViewModels = await query
+                .Include(p => p.User)
+                .Include(p => p.PostAttachments)
+                .Include(p => p.ForwardedPost.User)
+                .Include(p => p.ForwardedPost.PostAttachments)
+                .Include(p => p.ForwardedPost.ForwardedPost)
+                .OrderByDescending(p => p.Points)
+                .ToListAsync();
+            return _mapper.Map<List<PostViewModel>>(postViewModels);
         }
     }
 }
