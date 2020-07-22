2d46bccf12574d7dbe6a28ef6da7daa7df23d9b6
fuli1001-cmd <fuli1001@outlook.com>
2020-07-22 11:40:18 +0800

    fix

---
+++ Src/ApplicationMessages/Events/AppointmentScoreChangedEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationMessages.Events
+{
+    /// <summary>
+    /// 约拍值变化事件
+    /// </summary>
+    public class AppointmentScoreChangedEvent
+    {
+        public Guid UserId { get; set; }
+    }
+}
